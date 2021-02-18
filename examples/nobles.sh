09ee54e0eabee252dbd9b3fdd42d061ad6952bc4
leebr48 <leebr48@gmail.com>
2021-02-18 00:04:14 -0600

    Another intermediate push

--- examples/nobles.sh
+++ examples/nobles.sh
@@ -1,35 +1,56 @@
-#python3 example3_simple.py --ppp 20 --at-optimum --out ncsx4 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 2.5
-#python3 example3_simple.py --ppp 20 --at-optimum --out noble1 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *ncsx4* --frzCoils --iota_targ -0.381966 --QS_wt 2.5
-#python3 example3_simple.py --ppp 20 --at-optimum --out noble2 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *ncsx4* --frzCoils --iota_targ -0.206011 --QS_wt 2.5
-#python3 example3_simple.py --ppp 20 --at-optimum --out noble3 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *ncsx4* --frzCoils --iota_targ -0.618034 --QS_wt 2.5
-#python3 example3_simple.py --ppp 20 --at-optimum --out noble4 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *ncsx4* --frzCoils --iota_targ -0.793989 --QS_wt 2.5
-#python3 example3_simple.py --ppp 20 --at-optimum --out ncsx5 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 5
-#python3 example3_simple.py --ppp 20 --at-optimum --out noble1 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *ncsx5* --frzCoils --iota_targ -0.381966 --QS_wt 5
-#python3 example3_simple.py --ppp 20 --at-optimum --out noble2 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *ncsx5* --frzCoils --iota_targ -0.206011 --QS_wt 5
-#python3 example3_simple.py --ppp 20 --at-optimum --out noble3 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *ncsx5* --frzCoils --iota_targ -0.618034 --QS_wt 5
-#python3 example3_simple.py --ppp 20 --at-optimum --out noble4 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *ncsx5* --frzCoils --iota_targ -0.793989 --QS_wt 5
-#python3 example3_simple.py --ppp 20 --at-optimum --out ncsx6 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 7.5
-#python3 example3_simple.py --ppp 20 --at-optimum --out noble1 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *ncsx6* --frzCoils --iota_targ -0.381966 --QS_wt 7.5
-#python3 example3_simple.py --ppp 20 --at-optimum --out noble2 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *ncsx6* --frzCoils --iota_targ -0.206011 --QS_wt 7.5
-#python3 example3_simple.py --ppp 20 --at-optimum --out noble3 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *ncsx6* --frzCoils --iota_targ -0.618034 --QS_wt 7.5
-#python3 example3_simple.py --ppp 20 --at-optimum --out noble4 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *ncsx6* --frzCoils --iota_targ -0.793989 --QS_wt 7.5
-#python3 example3_simple.py --ppp 20 --out twoCloseOpt-1 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 1 --iota_targ -0.395938929522566 -0.381966
-#python3 example3_simple.py --ppp 20 --out twoCloseToOrig-1 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *twoCloseOpt-1_* --frzCoils --iota_targ -0.395938929522 --QS_wt 1
-#python3 example3_simple.py --ppp 20 --out twoCloseToNoble1-1 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *twoCloseOpt-1_* --frzCoils --iota_targ -0.381966 --QS_wt 1
-#python3 example3_simple.py --ppp 20 --out twoCloseOpt-5 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 5 --iota_targ -0.395938929522566 -0.381966
-#python3 example3_simple.py --ppp 20 --out twoCloseToOrig-5 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *twoCloseOpt-5* --frzCoils --iota_targ -0.395938929522 --QS_wt 5
-#python3 example3_simple.py --ppp 20 --out twoCloseToNoble1-5 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *twoCloseOpt-5* --frzCoils --iota_targ -0.381966 --QS_wt 5
+python3 simple.py --ppp 20 --out NEWallNobles3 --Nt_ma 6 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 1e-6 --curv 1e-6 --arclen 0.00 --QS_wt 10 --iota_targ -0.206011 -0.381966 -0.395938929522566 -0.618034 -0.793989 --Taylor
+python3 qfm.py --ppp 20 --Nt_ma 6 --Nt_coils 6 --sourcedir output-NEWallNobles3_nfp-3_ppp-20_Nt_ma-6_Nt_coils-6_curv-1e-06_tors-1e-06* --stellID 2
 
-#python3 example3_simple.py --ppp 20 --out twoCloseOpt-10 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 10 --iota_targ -0.395938929522566 -0.381966
-python3 example3_simple.py --ppp 20 --out twoCloseToOrig-10 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *twoCloseOpt-10_* --frzCoils --iota_targ -0.395938929522 --QS_wt 10
-python3 example3_simple.py --ppp 20 --out twoCloseToNoble1-10 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *twoCloseOpt-10_* --frzCoils --iota_targ -0.381966 --QS_wt 10
 
-#python3 example3_simple.py --ppp 20 --out twoFarOpt-1 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 1 --iota_targ -0.206011 -0.793989
-#python3 example3_simple.py --ppp 20 --out twoFarToOrig-1 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *twoFarOpt-1_* --frzCoils --iota_targ -0.206011 --QS_wt 1
-#python3 example3_simple.py --ppp 20 --out twoFarToNoble1-1 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *twoFarOpt-1_* --frzCoils --iota_targ -0.793989 --QS_wt 1
-#python3 example3_simple.py --ppp 20 --out twoFarOpt-5 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 5 --iota_targ -0.206011 -0.793989
-#python3 example3_simple.py --ppp 20 --out twoFarToOrig-5 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *twoFarOpt-5* --frzCoils --iota_targ -0.206011 --QS_wt 5
-#python3 example3_simple.py --ppp 20 --out twoFarToNoble1-5 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *twoFarOpt-5* --frzCoils --iota_targ -0.793989 --QS_wt 5
-#python3 example3_simple.py --ppp 20 --out twoFarOpt-10 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 10 --iota_targ -0.206011 -0.793989
-#python3 example3_simple.py --ppp 20 --out twoFarToOrig-10 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *twoFarOpt-10_* --frzCoils --iota_targ -0.206011 --QS_wt 10
-#python3 example3_simple.py --ppp 20 --out twoFarToNoble1-10 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --rld *twoFarOpt-10_* --frzCoils --iota_targ -0.793989 --QS_wt 10
+#python3 simple.py --ppp 20 --out test --Nt_ma 6 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 1e-2 --curv 1e-2 --arclen 0.00 --QS_wt 10 --iota_targ -0.395938929522566 --iter 5 --Taylor
+
+
+#python3 simple.py --ppp 20 --out twoClose --Nt_ma 6 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 1e-5 --curv 1e-5 --arclen 0.00 --QS_wt 10 --iota_targ -0.381966 -0.395938929522566 --Taylor
+
+
+#python3 simple.py --ppp 20 --out twoFar --Nt_ma 6 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 1e-2 --curv 1e-2 --arclen 0.00 --QS_wt 10 --iota_targ -0.206011 -0.793989 --Taylor
+
+
+#python3 simple.py --ppp 20 --out twoFar --Nt_ma 6 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 1e-3 --curv 1e-3 --arclen 0.00 --QS_wt 10 --iota_targ -0.206011 -0.793989 --Taylor
+
+
+#python3 simple.py --ppp 20 --out twoFar --Nt_ma 6 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 1e-4 --curv 1e-4 --arclen 0.00 --QS_wt 10 --iota_targ -0.206011 -0.793989 --Taylor
+
+
+#python3 simple.py --ppp 20 --out twoClose --Nt_ma 6 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 1e-4 --curv 1e-4 --arclen 0.00 --QS_wt 10 --iota_targ -0.381966 -0.395938929522566 --Taylor
+
+
+#python3 simple.py --ppp 20 --out reference --Nt_ma 6 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 1e-5 --curv 1e-5 --arclen 0.00 --QS_wt 10 --iota_targ -0.395938929522566 --Taylor
+
+
+#python3 simple.py --ppp 20 --out allNobles3 --Nt_ma 6 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 1e-3 --curv 1e-3 --arclen 0.00 --QS_wt 10 --iota_targ -0.206011 -0.381966 -0.395938929522566 -0.618034 -0.793989 --Taylor
+
+
+#python3 simple.py --ppp 20 --out reference --Nt_ma 6 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 1e-4 --curv 1e-4 --arclen 0.00 --QS_wt 10 --iota_targ -0.395938929522566 --Taylor
+#python3 simple.py --ppp 20 --out allNobles3 --Nt_ma 6 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 1e-2 --curv 1e-2 --arclen 0.00 --QS_wt 10 --iota_targ -0.206011 -0.381966 -0.395938929522566 -0.618034 -0.793989 --Taylor
+
+
+#python3 simple.py --ppp 20 --out allNobles3 --Nt_ma 6 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 1e-10 --curv 1e-10 --arclen 0.00 --QS_wt 10 --iota_targ -0.206011 -0.381966 -0.395938929522566 -0.618034 -0.793989 --Taylor
+
+
+#python3 simple.py --ppp 20 --out allNobles2 --Nt_ma 6 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 1e-4 --curv 1e-4 --arclen 0.00 --QS_wt 10 --iota_targ -0.206011 -0.381966 -0.395938929522566 -0.618034 -0.793989 --Taylor
+
+
+#python3 simple.py --ppp 20 --out allNobles --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 10 --iota_targ -0.206011 -0.381966 -0.395938929522566 -0.618034 -0.793989 --Taylor
+#python3 simple.py --ppp 20 --out allToNoble0 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 10 --iota_targ -0.206011 --frzCoils --rld output*allNobles* --stellID 0 --Taylor
+#python3 simple.py --ppp 20 --out allToNoble1 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 10 --iota_targ -0.381966 --frzCoils --rld output*allNobles* --stellID 1 --Taylor
+#python3 simple.py --ppp 20 --out allToNoble2 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 10 --iota_targ -0.395938929522566 --frzCoils --rld output*allNobles* --stellID 2 --Taylor
+#python3 simple.py --ppp 20 --out allToNoble3 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 10 --iota_targ -0.618034 --frzCoils --rld output*allNobles* --stellID 3 --Taylor
+#python3 simple.py --ppp 20 --out allToNoble4 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 10 --iota_targ 0.793989 --frzCoils --rld output*allNobles* --stellID 4 --Taylor
+
+##########################################################################################################################################################################################################
+
+#python3 simple.py --ppp 20 --out twoClose --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 10 --iota_targ -0.381966 -0.395938929522566 --Taylor
+#python3 simple.py --ppp 20 --out closeToNoble0 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 10 --iota_targ -0.381966 --frzCoils --rld output*twoClose* --stellID 0 --Taylor 
+#python3 simple.py --ppp 20 --out closeToNoble1 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 10 --iota_targ -0.395938929522566 --frzCoils --rld output*twoClose* --stellID 1 --Taylor 
+
+##########################################################################################################################################################################################################
+
+#python3 simple.py --ppp 20 --out twoFar --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 10 --iota_targ -0.206011 -0.793989 --Taylor
+#python3 simple.py --ppp 20 --out farToNoble0 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 10 --iota_targ -0.206011 --frzCoils --rld output*twoFar* --stellID 0 --Taylor 
+#python3 simple.py --ppp 20 --out farToNoble1 --Nt_ma 4 --Nt_coils 6 --min-dist 0.2 --dist-wt 1000. --tors 0.0 --curv 1e-6 --arclen 0.00 --QS_wt 10 --iota_targ -0.793989 --frzCoils --rld output*twoFar* --stellID 1 --Taylor 
