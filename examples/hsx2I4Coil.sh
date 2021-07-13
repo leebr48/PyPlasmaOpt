828852c8d5c084092f5ce353e64252811e5eac90
leebr48 <leebr48@gmail.com>
2021-07-13 15:45:34 -0500

    Cleanup

--- examples/hsx2I4Coil.sh
+++
@@ -1,3 +0,0 @@
-python3 simple.py --ppp 20 --out hsx2I4Coil --Nt_ma 6 --Nt_coils 6 --min_dist 0.1 --dist_wt 1000. --tors 0 --curv 1e-6 --arclen 0.00 --QS_wt 1 --iota_targ -0.395938929522566 -0.793989 --flat --num_coils 4 --nfp 4 --QFM 0 --maj_rad 1.2 --min_rad 0.3 --qfm_vol 0.44 --mmax 6 --nmax 6 --ntheta 40 --nphi 40 --Taylor --kick --z0factr 1 --mag 0.01 --image 10
-python3 qfm.py --sourcedir output-hsx2I4Coil* --stellID 0
-python3 qfm.py --sourcedir output-hsx2I4Coil* --stellID 1
--- examples/ncsx2INoQFM.sh
+++
@@ -1,3 +0,0 @@
-#python3 simple.py --ppp 20 --out ncsx2INoQFM --Nt_ma 6 --Nt_coils 6 --min_dist 0.2 --dist_wt 1000. --tors 0 --curv 1e-6 --arclen 0.00 --QS_wt 1 --iota_targ -0.395938929522566 -0.793989 --QFM 0 --qfm_vol 2.959 --mmax 6 --nmax 6 --ntheta 40 --nphi 40 --Taylor --image 10
-python3 qfm.py --sourcedir output-ncsx2INoQFM* --stellID 0 --qfm_vol 2.959
-python3 qfm.py --sourcedir output-ncsx2INoQFM* --stellID 1 --qfm_vol 2.959
